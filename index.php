8445c04c90a6a7bf0e25e3bc73f8d7a07ec9ba76
UlrichIvan <mtchokos@gmail.com>
2023-06-30 03:03:33 +0100

    Added before action to each prefix uri associate to an router has been done successfully

--- index.php
+++ index.php
@@ -2,9 +2,18 @@
 require_once(__DIR__ . "/vendor/autoload.php");
 
 use App\App;
+use App\Http\Request;
+use App\Http\Response;
 use App\Routers\Router;
 
 $app = new App();
+
+// add action before execute the any action associate to prefix uri
+$app->beforeIt("/api/users")->do(function (Request $req, Response $res) {
+      echo "beforeIt\n";
+});
+
+// add each router to his prefix uri
 
 $app->use("/api/users", function (): Router {
       return  userRouter();
--- src/app.php
+++ src/app.php
@@ -2,11 +2,10 @@
 
 namespace App;
 
-use App\Exception\RouterException;
 use App\Types\ArrayMap;
 use App\Routers\Router;
 use Closure;
-use InvalidArgumentException;
+use Exception;
 
 /**
  * manage application 
@@ -15,12 +14,17 @@ class App extends Router
 {
       public $name = "App";
 
+      private string $currentPrefix = "";
+
       private ArrayMap $routers;
 
+      private ArrayMap $beforeIts;
 
       function __construct()
       {
             $this->routers = new ArrayMap();
+            $this->beforeIts = new ArrayMap();
+
             parent::__construct();
       }
 
@@ -30,6 +34,30 @@ class App extends Router
             return $this;
       }
 
+      public function beforeIt(string $prefix): self
+      {
+            $this->setCurrentPrefix($prefix);
+
+            return $this;
+      }
+      /**
+       * @params Closure|array $cb [the callack or array of callback]
+       * @params string $prefix [the prefix uri]
+       */
+      public function do(Closure|array $cb, ?string $prefix = null): self
+      {
+            try {
+                  if ($this->currentPrefix === "" && empty($prefix)) {
+                        throw new Exception("An prefix value must not be empty,please set it before to added action to method " . __METHOD__, 1);
+                  }
+
+                  $this->beforeIts->add($prefix ? $prefix : $this->currentPrefix, is_callable($cb) ? [$cb] : [...$cb]);
+
+                  return $this;
+            } catch (\Throwable $th) {
+                  throw $th;
+            }
+      }
 
       public function run()
       {
@@ -41,7 +69,19 @@ class App extends Router
                   foreach ($this->routers as $prefix => $router) {
                         if (preg_match("#^" . $prefix . "#", $request_uri, $matched) && !empty($matched)) {
                               $uri = str_replace($matched[0], "", $request_uri);
+
+                              // if exists, execute beforeIts action associate to current prefix uri found
+
+                              if ($this->beforeIts->has($prefix)) {
+
+                                    foreach ($this->beforeIts->get($prefix) as $cb) {
+                                          call_user_func($cb, $this->request, $this->response);
+                                    }
+                              }
+
+                              // call router found associate to prefix uri
                               $router->readyGo($uri === "" ? "/" : $uri);
+
                               break;
                         }
                   }
@@ -49,5 +89,47 @@ class App extends Router
             } catch (\Throwable $th) {
                   throw $th;
             }
+      }
+
+
+
+      /**
+       * Get the value of routers
+       */
+      public function getRouters(): ArrayMap
+      {
+            return $this->routers;
+      }
+
+      /**
+       * Set the value of routers
+       *
+       * @return  self
+       */
+      public function setRouters(ArrayMap $routers)
+      {
+            $this->routers = $routers;
+
+            return $this;
+      }
+
+      /**
+       * Get the value of currentPrefix
+       */
+      public function getCurrentPrefix(): string
+      {
+            return $this->currentPrefix;
+      }
+
+      /**
+       * Set the value of currentPrefix
+       *
+       * @return  self
+       */
+      public function setCurrentPrefix(string $currentPrefix): self
+      {
+            $this->currentPrefix = $currentPrefix;
+
+            return $this;
       }
 }
