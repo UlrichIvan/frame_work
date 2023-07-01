ff2ed477b6da033067b635439ad8b7191ffd2953
UlrichIvan <mtchokos@gmail.com>
2023-07-01 07:37:47 +0100

    Added ArrayCalabale Type to set array of callback as parameters has been done successfully

--- src/interfaces/ArrayCallableInterface.php
+++ src/interfaces/ArrayCallableInterface.php
@@ -2,7 +2,10 @@
 
 namespace App\interface;
 
+use Closure;
+
 interface ArrayCallableInterface
 {
       public function isCallable(array $cbs): void;
+      public function add(Closure $cb): void;
 }
